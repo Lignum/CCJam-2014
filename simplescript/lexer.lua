38607c57290b0e3a5a0896d5757bb539b58dba72
GeorgeMcKinlay <grgmckinlay@gmail.com>
2014-06-13 19:05:23 +0200

    Got basic lexer to work.

--- simplescript/lexer.lua
+++ simplescript/lexer.lua
@@ -15,9 +15,7 @@ Lexer.__index = Lexer
 
 function Lexer.new()
 	local self = setmetatable({}, Lexer)
-	
 	self.parseTree = {}
-	
 	return self
 end
 
@@ -25,26 +23,28 @@ function Lexer:parseFunctionCall(funcCall)
 	local pkg, callInfo = funcCall:match("(%w+):(.+)")
 	callInfo = trim(callInfo)
 	
-	local tbl = { ["pkg"] = pkg, funcs = {} }
+	local tbl = { ["type"] = "call", ["pkg"] = pkg, funcs = {} }
 	
 	local function parseCallInfo(info)
-		info = info:gsub("%s-", "")
+		info = info:gsub("%s+", "")
 		local funcTable = {}
 		
 		for call in info:gmatch("[^,]+") do
 			local paramsTable = {}
-			local name,params,repetition = call:match("(%w+)%[?(.-)]?%*?(%d-)")
+			local name,params,repetition = call:match("(%w+)%s-%[(.-)]%s-%*?%s-(%d*)")
 			assert(name, "nameless function")
 			
-			params = params:gsub("%s-", "")
+			params = params:gsub("%s+", "")
 			
 			for param in params:gmatch("[^,]+") do
 				table.insert(paramsTable, param)
 			end
 			
-			funcTable["name"] = name
-			funcTable["params"] = paramsTable
-			funcTable["repetition"] = tonumber(repetition)
+			table.insert(funcTable, {
+				["name"] = name,
+				["params"] = paramsTable,
+				["repetition"] = tonumber(repetition)
+			})
 		end
 		
 		return funcTable
@@ -54,15 +54,21 @@ function Lexer:parseFunctionCall(funcCall)
 	return tbl
 end
 
-function Lexer:findFunctionCalls(line)
-	local tbl = {}
+function Lexer:getFunctionCall(line)
+	local call = line:match("%w+:.+")
+	return call
+end
+
+function Lexer:getStatement(line)
+	local statement = line:match("%w+%s-{%s-.-%s-}:")
+	return statement
+end
+
+function Lexer:parseStatement(line)
+	local type, condition = line:match("(%w+)%s-{%s-(.-)%s-}:")
+	condition = condition:gsub("%s+", "")
 	
-	for call in line:gmatch("(%w+):(.+)") do
-		local c = call:gsub("%s-", "")
-		table.insert(tbl, c)
-	end
-	
-	if #tbl == 0 then return nil end
+	local tbl = { ["type"] = "statement", ["kind"] = type, ["condition"] = condition }
 	return tbl
 end
 
@@ -76,15 +82,22 @@ function Lexer:tokenise(str)
 		lineCount = lineCount + 1
 		
 		local okay, err = pcall(function()
-			if not (startsWith(line, "//") or startsWith(line, "#") or startsWith(line, "--")) then		
-				if self:findFunctionCalls(line) ~= nil then
-					local func = self:parseFunctionCall(line)
+			if not startsWith(line, "//") and not startsWith(line, "#") and not startsWith(line, "--") then		
+				local statement = self:getStatement(line)
+				if statement ~= nil then
+					table.insert(self.parseTree, self:parseStatement(line))
+					return
+				end
+				
+				local call = self:getFunctionCall(line)
+				if call ~= nil then
+					table.insert(self.parseTree, self:parseFunctionCall(call))
 				end
 			end
 		end)
 		
 		if not okay then
-			return lineCount .. ":" .. err
+			return lineCount .. ": " .. err
 		end
 	end
 end
